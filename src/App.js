a08ad923b1e7560352b7809b4357739c244ae1a7
Marcin <mar.nowak@hotmail.com>
2023-08-06 13:49:57 +0200

    Working with CRUD, improving style in 'Todo.jsx'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { BsFillCalendar2PlusFill } from "react-icons/bs";
 import Todo from "./Todo";
+import { db } from "./firebase";
+import { query, collection, onSnapshot } from "firebase/firestore";
 
 function App() {
-  const [todos, setTodos] = useState(["abx", "dfg"]);
+  const [todos, setTodos] = useState([]);
+  // CRUD
+useEffect(() => {
+  const q = query(collection(db, "todos"));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    let todosArray = [];
+    querySnapshot.forEach((doc) => {
+      todosArray.push({...doc.data(), id: doc.id});
+    });
+    setTodos(todosArray);
+  })
+  return () => unsubscribe();
+}, [])
+
   return (
     <div className="h-screen w-screen p-4 bg-gradient-to-r from-[#2f58ed] to-[#2f988a]">
       <div className="bg-slate-200 max-w-[600px] w-full m-auto rounded-sm shadow-lg">
         <h2 className="text-2xl font-bold text-center text-gray-700 p-3">Todo application</h2>
         <form className="flex justify-between">
           <input className="border p-2 w-full text-lg" type="text" placeholder="Add an Todo" />
-          <button className="border p-4 ml-2 bg-purple-700 text-slate-400"><BsFillCalendar2PlusFill size={30} /></button>
+          <button className="border p-4 ml-2 bg-gray-100 text-slate-400"><BsFillCalendar2PlusFill size={30} /></button>
         </form>
         <ul>
           {todos.map((todo, index) => (
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { VscTrash } from "react-icons/vsc";
 
+const style = {
+  li: "flex justify-betwenn bg-slate-200 p-4 my-2 capitalize",
+  completedLi: "flex justify-between bg-slate-400 p-4 my-2 capitalize",
+  text: "ml-2 cursor-pinter",
+  completedText: "ml-2 cursor-pointer line through"
+}
 const Todo = ({todo}) => {
   return (
-    <li className="flex justify-between bg-slate-400 p-4 my-2 capitalize">
+    <li className={todo.completed ? style.completedLi : style.li}>
       <div className="flex">
-        <input type="checkbox" />
-        <p className="ml-2 cursor-pointer">{todo}</p>
+        <input type="checkbox" checked={todo.completed ? "checked" : ""} />
+        <p className={todo.completed ? style.completedText : style.text}>{todo.text}</p>
       </div>
       <button className="cursor-pointer flex items-center">{<VscTrash size={40} />}</button>
     </li>
