2345082de652d8fd602e4229c9a89deb4379003c
Marcin <mar.nowak@hotmail.com>
2023-08-06 12:33:18 +0200

    Installing react icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BsFillCalendar2PlusFill } from 'react-icons/bs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2>Todo application</h2>
+        <form>
+          <input type="text" placeholder="Add an Todo" />
+          <button ><BsFillCalendar2PlusFill size={30} /></button>
+        </form>
+      </div>
     </div>
   );
 }
